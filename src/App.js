2ece0518e50664e296eb5462b9ede24d72d20310
Kevin Hu <hukxchen+git@gmail.com>
2020-08-25 22:11:34 -0500

    entr works for hot reload by watching App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div id="page">
       <h1>Hello World</h1>
-      <h2>Live reload?</h2>
+      <h2>Live reload with entr?</h2>
+      <h3>Pretty neat! entr works!</h3>
+      <p>Why does it seem to be running more than 1 time per change?</p>
       <div id="nav"></div>
     </div>
   );
