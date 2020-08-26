431f35c48630a33785d1fb2ac5bdc9fa03e0e51c
Kevin Hu <hukxchen+git@gmail.com>
2020-08-26 07:03:59 -0500

    use react-router, set up minimal demo using BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div id="page">
-      <h1>Hello World</h1>
-      <h2>Live reload with entr?</h2>
-      <h3>Pretty neat! entr works!</h3>
-      <p>Why does it seem to be running more than 1 time per change?</p>
-      <div id="nav"></div>
+      <div id="nav">
+        <ul>
+          <li>
+            <Link to='/'>exact path</Link>
+          </li>
+          <li>
+            <Link to='/whatever'>the default path that matches anything</Link>
+          </li>
+        </ul>
+      </div>
+      {/*Switch component contains Routes and enables
+          comparing request URL with Route paths and 
+        will conditionally render the first matching path.
+          */}
+      <Switch>
+        <Route exact path="/">
+          <h3>You only got this because you requested '/' specifically</h3>
+        </Route>
+        <Route path="/">
+          <h4>You have hit the default path. It matches anything!</h4>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
-if (module.hot) {
-  module.hot.accept()
-}
+// Typical pattern for using react router
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('app')
+);
